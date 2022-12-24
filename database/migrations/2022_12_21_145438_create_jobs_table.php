f2041947acc547712b9b9c4d5d197f8dfe3a627e
Abdul Hafiidh Priyanto <hpabdul902@gmail.com>
2022-12-24 10:26:13 +0700

    ADD: Database Structure

---
+++ database/migrations/2022_12_21_145438_create_jobs_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\ForeignIdColumnDefinition;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('closed_date');
+            $table->string('job_location')->nullable();
+            $table->text('description');
+            $table->text('job_responsibilities');
+            $table->text('closing_statement');
+
+            $table->foreignId('company_id');
+            $table->foreignId('experience_id');
+            $table->foreignId('job_type_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
